e6519d79b04d2241aeacc1c073cfe67eee217460
Will <williamneal1@proton.me>
2024-02-27 20:40:01 +0100

    show only published posts

--- app/controllers/sitepress/site_controller.rb
+++ app/controllers/sitepress/site_controller.rb
@@ -14,7 +14,8 @@ module Sitepress
         layout.partial do
           render CollectionComponent.new(
             site.resources.glob("writing/*").select do |resource|
-              resource.data["status"] != "draft"
+              publish = resource.data.dig("publish")
+              publish && publish <= Date.today
             end
           )
         end
@@ -32,12 +33,6 @@ module Sitepress
         layout.partial do
           PhlexMarkdownComponent.new(page.body).call.html_safe
         end
-      end
-    end
-
-    def mastodon_layout(page)
-      ApplicationLayout.new do
-        render CollectionComponent.new(page.children)
       end
     end
 
