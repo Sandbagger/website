6c65526a734fe633beeea32bedba010f137d6184
Will <williamneal1@proton.me>
2024-04-20 14:16:28 +0200

    pixel caching on a per page basis

--- app/controllers/hit_controller.rb
+++ app/controllers/hit_controller.rb
@@ -5,10 +5,18 @@ class HitController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:handle]
   before_action :set_cache_headers
+  before_action :unique_id, only: [:handle]
 
   def handle
-    Rails.logger.info "Page view: #{request.query_parameters.inspect}, User Agent: #{request.user_agent}, Referer: #{request.referer}"
-
+    Hit.create(
+      unique_user_id: cookies[:unique_id],
+      user_agent: request.user_agent,
+      page: request.query_parameters[:page_id],
+      referer: request.referer,
+      session_id: request.session.id,
+      metadata: request.query_parameters,
+      path: params[:ref]
+    )
     send_data pixel,
       type: "image/gif",
       disposition: "inline",
@@ -23,8 +31,15 @@ class HitController < ApplicationController
 
   def set_cache_headers
     # set private cache instead of IP address to handle repeated views
+    page_key = params[:ref] || request.path
     seconds_until_midnight = (DateTime.now.end_of_day - DateTime.now).to_i
     headers["Cache-Control"] = "max-age=#{seconds_until_midnight}, private, must-revalidate"
     headers["Expires"] = 1.day.from_now.at_midnight.httpdate
+    # page key is crucial to ensure that the cache is unique per page
+    headers["Vary"] = "Accept, Accept-Encoding, #{page_key}"
+  end
+
+  def unique_id
+    cookies.permanent[:unique_id] ||= SecureRandomWrapper.hex(10).to_s
   end
 end
--- test/controllers/hit_controller_test.rb
+++ test/controllers/hit_controller_test.rb
@@ -1,12 +1,29 @@
 require "test_helper"
+require "securerandom"
 
-class HitControllerTest < ActionDispatch::IntegrationTest
-  test "should get hit" do
+class HitsControllerTest < ActionDispatch::IntegrationTest
+  include Mocktail::DSL  # Ensure DSL is included if not globally available
+  def setup
+    Hit.delete_all  # Ensuring each test starts with a clean slate
+    # Replace SecureRandom with a mock and stub the hex method
+
+    Mocktail.replace(SecureRandomWrapper)
+    stubs { SecureRandomWrapper.hex(10) }.with { "117bc6a90d678355f46a" }
+  end
+
+  test "sets a unique id cookie for a new visitor" do
     get hit_handle_url
+
     assert_response :success
-    assert_equal "image/gif", @response.content_type
-    assert_equal "inline", @response.headers["Content-Disposition"].split(";").first
-    expected_pixel_data = Base64.decode64("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/8AARgAsAmploiAAAAAASUVORK5CYII=")
-    assert_equal expected_pixel_data, @response.body
+    assert_equal "117bc6a90d678355f46a", cookies[:unique_id], "The unique_id cookie should be set correctly"
+  end
+
+  # cache headers are set to ensure that the tracking pixel is cached for the day
+  # so we assume that every request should be a new hit on a new day
+  test "should create a hit on" do
+    assert_difference("Hit.count", 1) do
+      get hit_handle_url, params: {page_id: "home"}, headers: {HTTP_REFERER: "http://example.com", HTTP_USER_AGENT: "Mozilla/5.0"}
+    end
+    assert_response :success
   end
 end
