6ef9a096b259c61ef0f86f588eb9ab66f25defaa
Will <williamneal1@proton.me>
2025-06-28 10:26:49 +0200

    Add 'logo' to nav

--- app/views/components/nav_component.rb
+++ app/views/components/nav_component.rb
@@ -5,14 +5,23 @@ class NavComponent < ApplicationComponent
 
   def template
     nav do
-      cluster do
-        Sitepress.site.resources.glob("*.html.*").each do |resource|
-          li do
-            link_to_page(resource)
+      ul(class: 'cluster-around') do
+        h3{ "William Neal" }
+
+        ul(class: 'cluster', role: "list") do
+          Sitepress.site.resources.glob("*.html.*").each do |resource|
+            li do
+              link_to_page(resource)
+            end
           end
-        end
-        li do
-          link_to "Mastodon", "https://ruby.social/@Sandbagger", target: "_blank", rel: "noopener noreferrer me"
+
+          li do
+            link_to "Toots", "https://ruby.social/@Sandbagger", target: "_blank", rel: "noopener noreferrer me"
+          end
+
+          li do
+            link_to "Skeets", "https://bsky.app/profile/williamneal.bsky.social", target: "_blank", rel: "noopener noreferrer me"
+          end
         end
       end
     end
