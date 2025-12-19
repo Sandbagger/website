a4630e0369466381cd21e2eea65d7b13388c75b8
William Neal <will@Williams-MacBook-Air.local>
2025-12-19 23:10:27 +0100

    added test

--- test/controllers/feed_controller_test.rb
+++ test/controllers/feed_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class FeedControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    # get feed_index_url
-    # assert_response :success
+    get feed_index_url(format: :xml)
+    assert_response :success
+    assert_includes response.content_type, "xml"
+    assert_includes response.body, "<rss"
   end
 end
