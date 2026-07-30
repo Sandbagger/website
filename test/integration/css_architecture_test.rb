4679820efb1c6749caf0a4ffd69a557062efd6af
William Neal <williamneal1@proton.me>
2026-07-30 22:59:45 +0200

    feat(css): establish curious editorial foundation

--- test/integration/css_architecture_test.rb
+++ test/integration/css_architecture_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class CssArchitectureTest < ActiveSupport::TestCase
   APPLICATION_CSS = Rails.root.join("app/assets/stylesheets/application.css")
+  VARIABLES_CSS = Rails.root.join("app/assets/stylesheets/global/variables.css")
+  RESET_CSS = Rails.root.join("app/assets/stylesheets/global/reset.css")
 
   test "application imports each CUBE layer once and in order" do
     imports = File.read(APPLICATION_CSS).scan(/@import url\(["']([^"']+)["']\);/).flatten
@@ -12,5 +14,22 @@ class CssArchitectureTest < ActiveSupport::TestCase
       "/utilities/index.css",
       "/blocks/index.css"
     ], imports
+  end
+
+  test "global tokens define the editorial foundation" do
+    tokens = File.read(VARIABLES_CSS)
+
+    [
+      "--color-paper: #f2eadb",
+      "--color-ink: #172c35",
+      "--color-coral: #d95136",
+      "--color-blush: #e8a7a0",
+      "--measure-reading: 68ch",
+      "--measure-wide: 75rem"
+    ].each { |token| assert_includes tokens, token }
+  end
+
+  test "reset does not add body padding" do
+    refute_match(/body\s*\{[^}]*padding:/m, File.read(RESET_CSS))
   end
 end
