8958a7b5fa1d01561abc3d5708bfc8679f44f52e
William Neal <williamneal1@proton.me>
2026-08-07 12:50:34 +0200

    feat(images): Add responsive cover variants

--- test/models/writing/cover_assets_test.rb
+++ test/models/writing/cover_assets_test.rb
@@ -13,18 +13,34 @@ class Writing::CoverAssetsTest < ActiveSupport::TestCase
     tag-overriding-in-phlex-and-markdown
     why-I-made-this-site-with-phlex-sitepress-and-rails
   ].freeze
+  SIZES = {480 => 252, 768 => 403, 1200 => 630}.freeze
 
-  test "repository contains canonical dimensioned WebP covers" do
+  test "repository retains canonical WebP cover masters during the responsive migration" do
     root = Rails.root.join("public/images/posts")
-    webps = root.glob("*.webp")
+    masters = EXPECTED.map { |slug| root.join("#{slug}.webp") }
 
-    assert_empty root.glob("*.svg")
-    assert_equal EXPECTED, webps.map { _1.basename(".webp").to_s }.sort
-    webps.each do |path|
+    masters.each do |path|
+      image = Sitepress::Image.new(path: path)
+
+      assert_path_exists path
+      assert_equal :webp, FastImage.type(path.to_s), path.to_s
+      assert_equal [1200, 630], [image.width, image.height], path.to_s
+    end
+  end
+
+  test "repository contains every responsive lossless WebP variant" do
+    root = Rails.root.join("public/images/posts")
+    expected = EXPECTED.product(SIZES.keys).map { |slug, width| "#{slug}-#{width}w.webp" }.sort
+    variants = root.glob("*-{480,768,1200}w.webp")
+
+    assert_equal expected, variants.map { _1.basename.to_s }.sort
+    variants.each do |path|
+      width = path.basename.to_s.match(/-(480|768|1200)w\.webp\z/)[1].to_i
       image = Sitepress::Image.new(path: path)
 
       assert_equal :webp, FastImage.type(path.to_s), path.to_s
-      assert_equal [1200, 630], [image.width, image.height], path.to_s
+      assert_equal "VP8L", path.binread(4, 12), path.to_s
+      assert_equal [width, SIZES.fetch(width)], [image.width, image.height], path.to_s
     end
   end
 end
